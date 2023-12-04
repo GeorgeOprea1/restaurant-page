a5133265032cec8f81f09692af6f0835119e9280
GeorgeOprea <oprea.george14@yahoo.com>
2023-12-04 17:27:11 +0200

    new paragraphs added to the contact section

--- src/contact.js
+++ src/contact.js
@@ -8,8 +8,24 @@ function createContact() {
   const address = document.createElement("p");
   address.textContent = "🏠 Cedar Koll Boulevard 42 / Wisconsin / USA";
 
+  const eMail = document.createElement("p");
+  eMail.textContent = "📩 louie-restaurant@yahoo.com";
+
+  const hours = document.createElement("div");
+  hours.classList.add("hours");
+
+  const weekHours = document.createElement("p");
+  weekHours.textContent = "🕒 Mon-Fri: 8am-8pm";
+  hours.appendChild(weekHours);
+
+  const weekendHours = document.createElement("p");
+  weekendHours.textContent = "Sat-Sun:8am-11pm";
+  hours.appendChild(weekendHours);
+
   contact.appendChild(address);
   contact.appendChild(phoneNumber);
+  contact.appendChild(eMail);
+  contact.appendChild(hours);
 
   return contact;
 }
